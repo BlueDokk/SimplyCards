d0d4b3c8b31fe6138c6d013a42140feedf37e064
BlueDokk <katzschwarz@gmail.com>
2021-06-03 11:40:26 -0500

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var p = {
+
+    btnToggler: document.querySelector('.navbar__toggler'),
+    navbar: document.querySelector('.navbar'),
+    btnUp: document.getElementById("btn-up")
+
+}
+
+var m = {
+
+    startApp: () => {
+        m.collapseMenu(p.btnToggler, p.navbar);
+        m.scroll();
+    },
+
+    collapseMenu: (element, target) => {
+        element.addEventListener("click", () => {
+            target.classList.toggle("collapsible--expanded");
+        })
+    },
+
+    scrollUp: () => {
+        var currentScroll = document.documentElement.scrollTop;
+
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(m.scrollUp);
+            window.scrollTo(0, currentScroll - (currentScroll / 10));
+        }
+    },
+    scroll: () => {
+        var scroll = document.documentElement.scrollTop;
+        if (p.btnUp != null) {
+            if (scroll > 100) {
+                p.btnUp.style.transform = 'scale(1)';
+            } else if (scroll < 100) {
+                p.btnUp.style.transform = 'scale(0)';
+            }
+        }
+    }
+}
+
+m.startApp();
+window.onscroll = m.scroll;
+
+document.getElementById("btn-up").addEventListener("click", m.scrollUp);
